b0dc1165837cf5cfc3ab230ce0c721ee82637cf3
HewkawAr <hewkawar@gmail.com>
2024-11-08 18:54:21 +0700

    Update Routes.ts

--- src/core/Routes.ts
+++ src/core/Routes.ts
@@ -1,7 +1,18 @@
+/**
+ * Generates a URL path for a Discord user.
+ * 
+ * @param userId - The unique identifier of the Discord user.
+ * @returns The URL path for the specified Discord user.
+ */
 function DiscordUser(userId: string) {
   return `/discord/users/${userId}`;
 }
 
+/**
+ * Generates a URL path for the Spotify API.
+ * 
+ * @returns The URL path for the Spotify API.
+ */
 function Spotify() {
   return `/spotify`;
 }
